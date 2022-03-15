913cc214d15913bba55a91ea8ae3d4d6e51c5250
lxqtpr <74341330+lxqtpr@users.noreply.github.com>
2022-03-15 23:59:34 +0400

    Update terms.tsx

--- client/pages/terms.tsx
+++ client/pages/terms.tsx
@@ -3,8 +3,10 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/header";
 import Nav from "../components/Nav/nav";
 import s from "../styles/Help.module.css";
+import {useAppSelector} from "../hooks/useTypedSelector";
 
 const Terms: React.FC = ({}): JSX.Element => {
+  const config =  useAppSelector(state => state.configSlice.config)
   return (
     <>
       <Header />
@@ -12,29 +14,9 @@ const Terms: React.FC = ({}): JSX.Element => {
       <div className={s.wrapper}>
         <div className={s.container}>
           <div className={s.help_inner}>
-            <div className={s.help_title}>Terms Of Service</div>
+            <div className={s.help_title}>{config.tof.tofTitle}</div>
             <div className={s.help_text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
-              venenatis convallis amet eu et ipsum. Ac, donec viverra tristique
-              elit euismod molestie uismod egestas egestas. Sollicitudin sed
-              adipiscing eu, ipsum purus et etiam mauris tristique. Consectetur
-              id pretium semper Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Id venenatis convallis amet eu et ipsum. Ac,
-              donec viverra tristique elit euismod molestie uismod egestas Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Id venenatis
-              convallis amet eu et ipsum. Ac, donec viverra tristique elit
-              euismod molestie uismod egestas egestas. Sollicitudin sed
-              adipiscing eu, ipsum purus et etiam mauris tristique. Consectetur
-              id pretium semper Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Id venenatis convallis amet eu et ipsum. Ac,
-              donec viverra tristique elit euismod molestie uismod egestas Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Id venenatis
-              convallis amet eu et ipsum. Ac, donec viverra tristique elit
-              euismod molestie uismod egestas egestas. Sollicitudin sed
-              adipiscing eu, ipsum purus et etiam mauris tristique. Consectetur
-              id pretium semper Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Id venenatis convallis amet eu et ipsum. Ac,
-              donec viverra tristique elit euismod molestie uismod egestas
+              {config.tof.description}
             </div>
           </div>
         </div>
